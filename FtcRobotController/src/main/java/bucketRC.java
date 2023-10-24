19a22ab9ef8121b9a6162320326732927452268b
programmer <atpham2008@gmail.com>
2023-10-24 19:50:53 -0400

    first attempt at intake object

---
+++ FtcRobotController/src/main/java/bucketRC.java
@@ -0,0 +1,36 @@
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class bucketRC {
+    public static double BUCKETSTART = .4;
+
+    public static double BUCKETEND = .6;
+
+    Servo bucketservo = null ;
+
+    public void init (){
+        HardwareMap hwMap = null;
+        bucketservo=hwMap.get(Servo.class,"bucketservo");
+    bucketservo.setPosition(BUCKETSTART);
+    }
+
+    public void bucketempty() {
+bucketservo.setPosition(BUCKETSTART);
+    }
+
+    public void bucketfull() {
+bucketservo.setPosition(BUCKETEND);
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
